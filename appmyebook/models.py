80c232997acc257d6bbfe25449e3f8caa6bc8bd3
Roberto <roberto.fernandez@opendeusto.es>
2023-11-03 20:17:06 +0100

    Creación de modelos y vistas iniciales
    Co-authored-by: luis-suarez-cuartas <luis-suarez-cuartas@users.noreply.github.com>

--- appmyebook/models.py
+++ appmyebook/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+ 
+class Genero(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+    
 
-# Create your models here.
+class Idioma(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField (Un id).
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=13)
+    descripcion = models.TextField()
+    genero = models.ForeignKey(Genero) # Un libro solo puede tener un género
+    idioma = models.ManyToManyField(Idioma) # Un libro puede estar en varios idiomas
+    reseñas = models.TextField()
+    def __str__(self):
+        return self.nombre
+
+
+
+
+   
--- appmyebook/views.py
+++ appmyebook/views.py
@@ -1,6 +1,17 @@
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Libro, Genero, Idioma
 
-def index(request):
-    return HttpResponse("Prueba")
-# Create your views here.
+#devuelve los datos de un libro
+def show_libro(request, libro_id):
+    libro = get_object_or_404(libro, pk=libro_id)
+    context = {'libro': libro }
+    return render(request, 'libro.html', context)
+    
+# devuelve los libros de un genero y un idioma
+def index_libro(request, genero_id, idioma_id):
+    genero = get_object_or_404(Genero, pk=genero_id)
+    idioma = get_object_or_404(Idioma, pk=idioma_id)
+    libros = get_list_or_404(Libro, genero=genero, idioma=idioma)
+    context = {'libros': libros}
+    return render(request, 'generoIdioma.html', context) #TODO generoIdioma.html no existe
